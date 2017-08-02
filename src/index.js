ea0ee4069869ac37324468bf20c918bcf33ff84c
Ivan5646 <Mike000027@yandex.ru>
2017-08-02 15:29:52 +0700

    component accepts objects

--- src/index.js
+++ src/index.js
@@ -11,20 +11,45 @@ import registerServiceWorker from './registerServiceWorker';
 //   document.getElementById('root')
 // );
 
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;
+function formatDate(date) {
+  return date.toLocaleDateString();
 }
 
-function App() {
+function Comment(props) {
   return (
-    <div>
-      <Welcome name="Sara" />
-      <Welcome name="Cahal" />
-      <Welcome name="Edite" />
+    <div className="Comment">
+      <div className="UserInfo">
+        <img className="Avatar"src={props.author.avatarUrl} alt={props.author.name} />
+        <div className="UserInfo-name">
+          {props.author.name}
+        </div>
+      </div>
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
     </div>
   );
 }
 
-ReactDOM.render(<App />,document.getElementById('root'));
+const comment = {
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl: 'http://placekitten.com/g/64/64'
+  }
+};
+
+ReactDOM.render(
+  <Comment
+    date={comment.date} // date - props.date of the Comment?
+    text={comment.text} // props.text ...
+    author={comment.author} />, // props.author
+  document.getElementById('root')
+);
+
 
 registerServiceWorker();
