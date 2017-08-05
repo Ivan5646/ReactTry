cde9501c7c5e32853b681d569f60209b7b2e5fa6
Ivan5646 <Mike000027@yandex.ru>
2017-08-05 20:20:45 +0700

    passing data error

--- src/index.js
+++ src/index.js
@@ -4,7 +4,34 @@ import './index.css';
 // import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+class TaskRow extends React.Component {
+  render () {
+    return (
+      <tr>
+        <td>{this.props.tasks.task}</td> {/* should pass taskArray at some point... */}
+        <td>{this.props.tasks.completed}</td>
+      </tr>
+    );
+  }
+}
 
-data = [{task: "read a book", completed: "false"}, {task: "go to the gym", true: "false"}, {task: "go for a walk", completed: "false"}]; // commit
+class Table extends React.Component {
+  render () {
+    return (
+      <table>
+        
+      </table>
+    );
+  }
+}
 
-registerServiceWorker();
+
+ReactDOM.render(
+  <TaskRow tasks={taskArray} />,
+  document.getElementById('root') // changed to root
+);
+
+
+var taskArray = [{task: "read a book", completed: "false"}, {task: "go to the gym", true: "false"}, {task: "go for a walk", completed: "false"}]; 
+
+registerServiceWorker(); // what is it for?
