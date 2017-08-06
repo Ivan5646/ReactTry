84972187402f99734dfb7e947ca61f04d426fd84
Ivan5646 <Mike000027@yandex.ru>
2017-08-06 15:58:43 +0700

    table displayed

--- src/index.js
+++ src/index.js
@@ -4,22 +4,23 @@ import './index.css';
 // import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-// class TaskRow extends React.Component {
-//   render () {
-//     return (
-//       <tr>
-//         <td>{JSON.stringify(this.props.tasks)}</td> {/* should pass taskArray at some point... */}
-//         <td>table cell</td>
-//       </tr>
-//     );
-//   }
-// }
+class TaskRow extends React.Component {
+  render () {
+    return (
+      <tr>
+        <td>{this.props.task.task}</td> {/* should pass taskArray at some point... */}
+        <td>{this.props.task.completed}</td>
+      </tr>
+    );
+  }
+}
 
 class Table extends React.Component {
   render () {
     var rows = [];
     this.props.tasks.forEach(function(task){
-      rows.push(task.task, task.completed);
+      //rows.push(task.task, task.completed);
+      rows.push(<TaskRow task={task}/>)
     });
     console.log(rows);
     return (
