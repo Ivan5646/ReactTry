1fa0f463dab96d6bbea453a8b4fddf9df31f6ba2
Ivan5646 <Mike000027@yandex.ru>
2017-08-06 15:51:09 +0700

    passed array displayed

--- src/index.js
+++ src/index.js
@@ -4,32 +4,37 @@ import './index.css';
 // import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-class TaskRow extends React.Component {
-  render () {
-    return (
-      <tr>
-        <td>{JSON.stringify(this.props.tasks)}</td> {/* should pass taskArray at some point... */}
-        <td>table cell</td>
-      </tr>
-    );
-  }
-}
-
-// class Table extends React.Component {
+// class TaskRow extends React.Component {
 //   render () {
 //     return (
-//       <table>
-        
-//       </table>
+//       <tr>
+//         <td>{JSON.stringify(this.props.tasks)}</td> {/* should pass taskArray at some point... */}
+//         <td>table cell</td>
+//       </tr>
 //     );
 //   }
 // }
 
+class Table extends React.Component {
+  render () {
+    var rows = [];
+    this.props.tasks.forEach(function(task){
+      rows.push(task.task, task.completed);
+    });
+    
+    return (
+      <table>
+        <tbody>{rows}</tbody>
+      </table>
+    );
+  }
+}
+
 var taskArray = [{task: "read a book", completed: "false"}, {task: "go to the gym", true: "false"}, {task: "go for a walk", completed: "false"}]; 
 
 ReactDOM.render(
-  <TaskRow tasks={taskArray}/>,
-  document.getElementById('root') // changed to root
+  <Table tasks={taskArray}/>,
+  document.getElementById('root') 
 );
 
 
