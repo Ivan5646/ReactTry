8cb93c14a47d1ccc9c45cd2e8ca181b547494c62
Ivan5646 <Mike000027@yandex.ru>
2017-08-07 20:13:20 +0700

    change task status

--- src/index.js
+++ src/index.js
@@ -4,8 +4,24 @@ import './index.css';
 // import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+class Completed extends React.Component {
+  constructor(props) {
+    super(props); // don't know what it is for
+    //this.state = {isToggleOn: true};
+    this.chagneStatus = this.chagneStatus.bind(this); // This binding is necessary to make `this` work in the callback
+  }
+  chagneStatus() {
+    console.log("task status has been changed");
+  }
+  render() {
+    return (
+      <input type="checkbox" onClick={this.chagneStatus}/>
+    )
+  }
+}
+
 class SearchBar extends React.Component { // search implement later first do the checkbox
-  render () {
+  render() {
     return (
         <form>
           <input type="text" placeholder="Search..."/> 
@@ -16,18 +32,18 @@ class SearchBar extends React.Component { // search implement later first do the
 }
 
 class TaskRow extends React.Component {
-  render () {
+  render() {
     return (
       <tr>
         <td>{this.props.taskUnit.task}</td> {/* taskArray passed in the Table component */}
-        <td>{this.props.taskUnit.completed}</td>
+        <td><Completed/></td> {/* add here? checkbox with event listener or component with checkbox and event listener */}
       </tr>
     );
   }
 }
 
 class Table extends React.Component {
-  render () {
+  render() {
     var rows = [];
     this.props.tasks.forEach(function(taskUnit){
       rows.push(<TaskRow taskUnit={taskUnit} key={taskUnit.task}/>) // should have key property, otherwise giving warning
@@ -42,7 +58,7 @@ class Table extends React.Component {
 }
 
 class FilterableTable extends React.Component {
- render () {
+ render() {
    return (
       <div>
         <SearchBar/>
